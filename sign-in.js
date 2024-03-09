f4c7c9cf15711fc468a0302fe62f0cb4b800c14d
Mahidhar Reddy G <mahidhar.reddy2003@gmail.com>
2024-03-09 22:40:20 +0530

    Adds Sign in Page
    Pre Final

--- sign-in.js
+++ sign-in.js
@@ -10,3 +10,21 @@ emailInput.addEventListener('blur', () => {
         emailLabel.classList.remove('has-value');
     }
 });
+
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password");
+    var showPasswordIcon = document.getElementById("show-password");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        showPasswordIcon.src = "images/hide_pass.png"; 
+    } else {
+        passwordInput.type = "password";
+        showPasswordIcon.src = "images/show_pass.png"; 
+    }
+}
+
+document.addEventListener('selectstart',(e) =>{
+    e.preventDefault();
+})
+
